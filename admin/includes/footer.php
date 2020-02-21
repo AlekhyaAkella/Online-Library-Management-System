f012153096a01bfcba6d0a5498d4d582d260334a
Alekhya Akella <47712386+AlekhyaAkella@users.noreply.github.com>
2020-02-21 12:55:11 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Knowledge Hub Management System | Designed by : KIETSTUDENT</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
